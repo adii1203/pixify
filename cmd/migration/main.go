202205110cd5b5c63b40359af3e7dafc98274a56
adii1203 <aditya32ft@gmail.com>
2025-02-01 22:20:50 +0530

    Add migration setup and File model definition

---
+++ cmd/migration/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/adii1203/pixify/data"
+	"github.com/adii1203/pixify/db"
+)
+
+func main() {
+	db := db.Init()
+	db.AutoMigrate(&data.File{})
+}
---
+++ data/file.go
@@ -0,0 +1,14 @@
+package data
+
+import "gorm.io/gorm"
+
+type File struct {
+	gorm.Model
+	Path     string `json:"path"`
+	Name     string `json:"name"`
+	FileType string `json:"file_type"`
+	MimeType string `json:"mime_type"`
+	Size     uint64 `json:"size"`
+	Width    uint64 `json:"width"`
+	Height   uint64 `json:"height"`
+}
