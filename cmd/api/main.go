35ad602512112236eee82963d0812661ec5a1ce0
adii1203 <aditya32ft@gmail.com>
2025-02-01 22:19:51 +0530

    Implement S3 client initialization and graceful server shutdown in main API

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,16 +1,38 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
+	"github.com/adii1203/pixify/handler"
+	"github.com/adii1203/pixify/storage"
+	"github.com/adii1203/pixify/utils"
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/gommon/log"
 )
 
 func main() {
+	// loading environment variables
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
 
+	// initializing s3 client
+	fmt.Println("initializing s3 client...")
+	S3, err := storage.NewS3Client()
+	if err != nil {
+		log.Printf("error : %v", err.Error())
+	}
+
+	// initializing echo server
 	e := echo.New()
+	e.Validator = utils.NewValidator(context.TODO())
 	e.Use(middleware.RecoverWithConfig(middleware.RecoverConfig{
 		StackSize: 1 << 10,
 		LogLevel:  log.ERROR,
@@ -23,5 +45,25 @@ func main() {
 		return c.String(http.StatusOK, "Hello World!")
 	})
 
-	e.Logger.Fatal(e.Start(":8080"))
+	e.POST("/api/upload", handler.HandelPutObjectPresignURL(S3))
+	e.GET("/images/:key", handler.HandelGetImage())
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	// start server
+	go func() {
+		if err := e.Start(":8080"); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal("shutting down the server")
+		}
+	}()
+
+	// gracefully shut down the server with a timeout
+	<-ctx.Done()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
+
 }
