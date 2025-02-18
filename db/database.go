0aca479d55854e01d14152a16fdaf07ce6f2bef8
adii1203 <aditya32ft@gmail.com>
2025-02-18 23:12:36 +0530

    Refactor database initialization to use GormDB struct and add InsertFile method for file insertion

--- db/database.go
+++ db/database.go
@@ -5,12 +5,27 @@ import (
 	"log"
 	"os"
 
+	"github.com/adii1203/pixify/data"
 	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-var Db *gorm.DB
+type Store interface {
+	InsertFile(file *data.File) error
+}
+
+type GormDB struct {
+	Db *gorm.DB
+}
+
+func (g *GormDB) InsertFile(file *data.File) error {
+	tx := g.Db.Create(file)
+	if tx.Error != nil {
+		return fmt.Errorf("error inserting file: %v", tx.Error)
+	}
+	return nil
+}
 
 func connectDatabase() (*gorm.DB, error) {
 	dns := os.Getenv("DB_DNS")
@@ -22,7 +37,7 @@ func connectDatabase() (*gorm.DB, error) {
 	return db, nil
 }
 
-func Init() *gorm.DB {
+func Init() *GormDB {
 	if err := godotenv.Load(); err != nil {
 		log.Panic("error loading env", err)
 	}
@@ -30,5 +45,7 @@ func Init() *gorm.DB {
 	if err != nil {
 		log.Fatal(err)
 	}
-	return db
+	return &GormDB{
+		Db: db,
+	}
 }
