f3cf5905baae45df56fb6df58dacd53e9f400076
adii1203 <aditya32ft@gmail.com>
2025-02-18 23:12:53 +0530

    Add image processing and database insertion in image upload handler

--- handler/handler.go
+++ handler/handler.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"bytes"
 	"context"
 	"errors"
 	"fmt"
@@ -9,9 +10,12 @@ import (
 	"mime/multipart"
 	"net/http"
 
+	"github.com/adii1203/pixify/data"
+	"github.com/adii1203/pixify/db"
 	"github.com/adii1203/pixify/storage"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/h2non/bimg"
 	"github.com/labstack/echo/v4"
 )
 
@@ -19,7 +23,7 @@ type PutObjectKey struct {
 	File *multipart.FileHeader `form:"file" json:"file" validate:"required"`
 }
 
-func HandelPutImage(s *storage.BucketBasics) echo.HandlerFunc {
+func HandlePutImage(s *storage.BucketBasics, db *db.GormDB) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		var objectKey PutObjectKey
 		err := c.Bind(&objectKey)
@@ -40,25 +44,66 @@ func HandelPutImage(s *storage.BucketBasics) echo.HandlerFunc {
 			})
 		}
 
+		if objectKey.File == nil {
+			return c.JSON(http.StatusBadRequest, map[string]string{
+				"message": "file is required",
+			})
+		}
+
 		f, err := objectKey.File.Open()
 		if err != nil {
 			return c.JSON(http.StatusInternalServerError, map[string]string{
-				"message": "error file opening",
+				"message": fmt.Errorf("error opening file: %v", err).Error(),
 			})
 		}
-		f.Close()
+		defer f.Close()
+
+		buf, err := io.ReadAll(f)
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, map[string]string{
+				"message": fmt.Errorf("error reading file: %v", err).Error(),
+			})
+		}
+
+		img := bimg.NewImage(buf)
+		meta, err := img.Metadata()
+
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, map[string]string{
+				"message": fmt.Errorf("error getting image size: %v", err).Error(),
+			})
+		}
 
 		_, err = s.S3Client.PutObject(context.Background(), &s3.PutObjectInput{
 			Bucket: aws.String("pixify-raw-images-bucket"),
 			Key:    aws.String(objectKey.File.Filename),
-			Body:   f,
+			Body:   bytes.NewReader(buf),
 		})
 		if err != nil {
-			log.Fatal(err)
+			return c.JSON(http.StatusInternalServerError, map[string]string{
+				"message": fmt.Errorf("error uploading file: %v", err).Error(),
+			})
 		}
 
+		file := data.File{
+			Name:     objectKey.File.Filename,
+			Path:     fmt.Sprint("/", objectKey.File.Filename),
+			Size:     uint64(len(buf)),
+			Width:    uint64(meta.Size.Width),
+			Height:   uint64(meta.Size.Height),
+			MimeType: meta.Type,
+			Url:      fmt.Sprintf("https://d28dzh6o5yikbs.cloudfront.net/%s", objectKey.File.Filename),
+			FileType: objectKey.File.Header.Get("Content-Type"),
+		}
+		err = db.InsertFile(&file)
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, map[string]string{
+				"message": fmt.Errorf("error saving file: %v", err).Error(),
+			})
+		}
 		return c.JSON(http.StatusOK, map[string]interface{}{
 			"status": "ok",
+			"file":   file,
 		})
 	}
 }
