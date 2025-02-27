8100d1b387c41b9b3aaec16a7a803dbea62fe558
adii1203 <aditya32ft@gmail.com>
2025-02-27 19:57:41 +0530

    Enhance S3 client initialization with static credentials and region configuration

--- storage/s3.go
+++ storage/s3.go
@@ -3,8 +3,11 @@ package storage
 import (
 	"context"
 	"log"
+	"os"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
@@ -14,11 +17,14 @@ type BucketBasics struct {
 }
 
 func NewS3Client() (*BucketBasics, error) {
-	cfg, err := config.LoadDefaultConfig(context.TODO())
+	creds := aws.NewCredentialsCache(credentials.NewStaticCredentialsProvider(os.Getenv("AWS_ACCESS_KEY"), os.Getenv("AWS_SECRET_KEY"), ""))
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
+		config.WithCredentialsProvider(creds),
+		config.WithRegion(os.Getenv("AWS_REGION")),
+	)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	client := s3.NewFromConfig(cfg)
 	return &BucketBasics{
 		S3Client:      client,
